d037d6f664efe1387d5e72928bb8be103f6716c2
jillurcf <jrtushar@gmail.comm>
2023-10-30 06:55:59 -0700

    jwt access token created for specific user

---
+++ __jwt__steps.js
@@ -0,0 +1,24 @@
+/**
+ * install jsonwebtoken
+ * jwt.sign (payload, secret, {expiresIn:})
+ * token client
+ */
+
+/**
+ * how to store token in the client side
+ * 1. memory --> ok type
+ * 2. local storage --> ok type (xss)
+ * 3. cookies: http only
+ * 
+ */
+
+/**
+ * 1. set cookies http only. cookie for development secure: false,
+ * 2. cors
+ * app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
+ * 3. client side axios setting
+   4. axios set with credential
+ */
